3d4a0f4c0d2221e3fbc31044a450ad698e5504e3
kawser00 <access.kawser@gmail.com>
2025-12-30 13:00:11 +0600

    feat: add login ui

---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,63 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import Field from "../common/Field";
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+    navigate("/");
+  };
+
+  return (
+    <form
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-15"
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <Field label="Email" error={errors.email}>
+        <input
+          {...register("email", { required: "Email ID is Required" })}
+          className={`auth-input ${
+            errors.email ? "border-red-500" : "border-gray-200"
+          }`}
+          type="email"
+          name="email"
+          id="email"
+        />
+      </Field>
+
+      <Field label="Password" error={errors.password}>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Your password must be at least 8 characters",
+            },
+          })}
+          className={`auth-input ${
+            errors.password ? "border-red-500" : "border-gray-200"
+          }`}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </Field>
+
+      <Field>
+        <button className="auth-input bg-lws-green font-bold text-deep-dark transition-all hover:opacity-90 cursor-pointer">
+          Login
+        </button>
+      </Field>
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/common/Field.jsx
@@ -0,0 +1,30 @@
+import { Children } from "react";
+
+const Field = ({ label, children, htmlFor, error }) => {
+  const id = htmlFor || getChildId(children);
+  return (
+    <div className="form-control">
+      {label && (
+        <label htmlFor={id} className="auth-label">
+          {label}
+        </label>
+      )}
+      {children}
+      {!!error && (
+        <div role="alert" className="text-red-600">
+          {error.message}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const getChildId = (children) => {
+  const child = Children.only(children);
+
+  if (child?.props && "id" in child.props) {
+    return child.props.id;
+  }
+};
+
+export default Field;
