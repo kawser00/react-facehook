72be325e9667e92a2d5d2b1500bb0bbc8c022062
kawser00 <access.kawser@gmail.com>
2026-01-02 22:02:48 +0600

    feat: add posts in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,31 @@
-import { Link } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import PostList from "../components/posts/PostList";
+import { useAxios } from "../hooks/useAxios";
 
 const HomePage = () => {
+  const { api } = useAxios();
+
+  const fetchPosts = async ({ queryKey }) => {
+    const response = await api.get(`/${queryKey[0]}`);
+    return response.data;
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) {
+    return <div>Fetching posts...</div>;
+  }
+
+  if (error) {
+    return <div className="text-red-500">Error fetching posts! {error.message}</div>;
+  }
+
   return (
     <div>
-      <p>HomePage</p>
-      <Link to="/me">Go to Profile Page</Link>
+      <PostList posts={data || []} />
     </div>
   );
 };
